dc7abdbda188de23479dbddd7f87171fba1ad047
Alex Hazniuk <ohazniuk@gmail.com>
2023-09-23 13:55:03 +0300

    implemented task 'Pagination, Sorting, and Swagger'

--- src/main/java/com/example/onlinebookstore/service/BookService.java
+++ src/main/java/com/example/onlinebookstore/service/BookService.java
@@ -4,6 +4,7 @@ import com.example.onlinebookstore.dto.BookDto;
 import com.example.onlinebookstore.dto.CreateBookRequestDto;
 import com.example.onlinebookstore.search.BookSearchParameters;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
@@ -12,7 +13,7 @@ public interface BookService {
 
     BookDto update(Long id, CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     void deletedById(Long id);
 
